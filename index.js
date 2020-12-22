fbe99175e328c6b029d9aff5a4390f9c48de209c
joko7 <jokosengono@gmail.com>
2020-12-22 18:59:34 +0700

    Updated for M2 BOT

--- index.js
+++ index.js
@@ -4,91 +4,91 @@ const options = require('./utils/options')
 const { color, messageLog } = require('./utils')
 const HandleMsg = require('./HandleMsg')
 
-const start = (aruga = new Client()) => {
+const start = (m2 = new Client()) => {
     console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
-    console.log(color(figlet.textSync('ARUGA BOT', { font: 'Ghost', horizontalLayout: 'default' })))
+    console.log(color(figlet.textSync('M2 BOT', { font: 'Ghost', horizontalLayout: 'default' })))
     console.log(color(figlet.textSync('----------------', { horizontalLayout: 'default' })))
-    console.log(color('[DEV]'), color('ArugaZ', 'yellow'))
+    console.log(color('[DEV]'), color('M2', 'yellow'))
     console.log(color('[~>>]'), color('BOT Started!', 'green'))
 
     // Mempertahankan sesi agar tetap nyala
-    aruga.onStateChanged((state) => {
+    m2.onStateChanged((state) => {
         console.log(color('[~>>]', 'red'), state)
-        if (state === 'CONFLICT' || state === 'UNLAUNCHED') aruga.forceRefocus()
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') m2.forceRefocus()
     })
 
     // ketika bot diinvite ke dalam group
-    aruga.onAddedToGroup(async (chat) => {
-	const groups = await aruga.getAllGroups()
+    m2.onAddedToGroup(async (chat) => {
+	const groups = await m2.getAllGroups()
 	// kondisi ketika batas group bot telah tercapai,ubah di file settings/setting.json
 	if (groups.length > groupLimit) {
-	await aruga.sendText(chat.id, `Sorry, the group on this Bot is full\nMax Group is: ${groupLimit}`).then(() => {
-	      aruga.leaveGroup(chat.id)
-	      aruga.deleteChat(chat.id)
+	await m2.sendText(chat.id, `Sorry, the group on this Bot is full\nMax Group is: ${groupLimit}`).then(() => {
+	      m2.leaveGroup(chat.id)
+	      m2.deleteChat(chat.id)
 	  }) 
 	} else {
 	// kondisi ketika batas member group belum tercapai, ubah di file settings/setting.json
 	    if (chat.groupMetadata.participants.length < memberLimit) {
-	    await aruga.sendText(chat.id, `Sorry, Bot comes out if the group members do not exceed ${memberLimit} people`).then(() => {
-	      aruga.leaveGroup(chat.id)
-	      aruga.deleteChat(chat.id)
+	    await m2.sendText(chat.id, `Sorry, Bot comes out if the group members do not exceed ${memberLimit} people`).then(() => {
+	      m2.leaveGroup(chat.id)
+	      m2.deleteChat(chat.id)
 	    })
 	    } else {
-        await aruga.simulateTyping(chat.id, true).then(async () => {
-          await aruga.sendText(chat.id, `Hai minna~, Im Aruga Bot. To find out the commands on this bot type ${prefix}menu`)
+        await m2.simulateTyping(chat.id, true).then(async () => {
+          await m2.sendText(chat.id, `Hi buddy~, I m M2 BOT but you can call me M. To find out the commands for this bot services type ${prefix}menu or ${prefix}help`)
         })
 	    }
 	}
     })
 
     // ketika seseorang masuk/keluar dari group
-    aruga.onGlobalParicipantsChanged(async (event) => {
-        const host = await aruga.getHostNumber() + '@c.us'
+    m2.onGlobalParicipantsChanged(async (event) => {
+        const host = await m2.getHostNumber() + '@c.us'
 		const welcome = JSON.parse(fs.readFileSync('./settings/welcome.json'))
 		const isWelcome = welcome.includes(event.chat)
-		let profile = await aruga.getProfilePicFromServer(event.who)
+		let profile = await m2.getProfilePicFromServer(event.who)
 		if (profile == '' || profile == undefined) profile = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTQcODjk7AcA4wb_9OLzoeAdpGwmkJqOYxEBA&usqp=CAU'
         // kondisi ketika seseorang diinvite/join group lewat link
         if (event.action === 'add' && event.who !== host && isWelcome) {
-			await aruga.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
-            await aruga.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us✨`)
+			await m2.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
+            await m2.sendTextWithMentions(event.chat, `Hello, Welcome to the group @${event.who.replace('@c.us', '')} \n\nHave fun with us ✨`)
         }
         // kondisi ketika seseorang dikick/keluar dari group
         if (event.action === 'remove' && event.who !== host) {
-			await aruga.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
-            await aruga.sendTextWithMentions(event.chat, `Good bye @${event.who.replace('@c.us', '')}, We'll miss you✨`)
+			await m2.sendFileFromUrl(event.chat, profile, 'profile.jpg', '')
+            await m2.sendTextWithMentions(event.chat, `Good bye @${event.who.replace('@c.us', '')}, We'll miss you 😭`)
         }
     })
 
-    aruga.onIncomingCall(async (callData) => {
+    m2.onIncomingCall(async (callData) => {
         // ketika seseorang menelpon nomor bot akan mengirim pesan
-        await aruga.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
+        await m2.sendText(callData.peerJid, 'Maaf sedang tidak bisa menerima panggilan.\n\n-bot')
         .then(async () => {
             // bot akan memblock nomor itu
-            await aruga.contactBlock(callData.peerJid)
+            await m2.contactBlock(callData.peerJid)
         })
     })
 
     // ketika seseorang mengirim pesan
-    aruga.onMessage(async (message) => {
-        aruga.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
+    m2.onMessage(async (message) => {
+        m2.getAmountOfLoadedMessages() // menghapus pesan cache jika sudah 3000 pesan.
             .then((msg) => {
                 if (msg >= 3000) {
-                    console.log('[aruga]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
-                    aruga.cutMsgCache()
+                    console.log('[m2]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
+                    m2.cutMsgCache()
                 }
             })
-        HandleMsg(aruga, message)    
+        HandleMsg(m2, message)    
     
     })
 	
     // Message log for analytic
-    aruga.onAnyMessage((anal) => { 
+    m2.onAnyMessage((anal) => { 
         messageLog(anal.fromMe, anal.type)
     })
 }
 
 //create session
 create(options(true, start))
-    .then((aruga) => start(aruga))
+    .then((m2) => start(m2))
     .catch((err) => new Error(err))
